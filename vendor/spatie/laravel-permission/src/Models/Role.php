ccf9920e3d1d104c60a0bdf1e39219a699b26cfb
gergesbarakat <girgisbarakat70@gmail.com>
2025-05-22 18:53:59 +0300

    fhhjjg

--- vendor/spatie/laravel-permission/src/Models/Role.php
+++ vendor/spatie/laravel-permission/src/Models/Role.php
@@ -43,10 +43,9 @@ class Role extends Model implements RoleContract
 
     public static function create(array $attributes = [])
     {
-
         $attributes['guard_name'] = $attributes['guard_name'] ?? Guard::getDefaultName(static::class);
 
-        $params = ['name' => $attributes['name'], 'guard_name' => $attributes['guard_name'],'parent_id'=>$attributes['parent_id']];
+        $params = ['name' => $attributes['name'], 'guard_name' => $attributes['guard_name']];
         if (PermissionRegistrar::$teams) {
             if (array_key_exists(PermissionRegistrar::$teamsKey, $attributes)) {
                 $params[PermissionRegistrar::$teamsKey] = $attributes[PermissionRegistrar::$teamsKey];
@@ -55,7 +54,7 @@ class Role extends Model implements RoleContract
             }
         }
         if (static::findByParam($params)) {
-            throw RoleAlreadyExists::create($attributes['name'], $attributes['guard_name'],$attributes['parent_id']);
+            throw RoleAlreadyExists::create($attributes['name'], $attributes['guard_name']);
         }
 
         return static::query()->create($attributes);
